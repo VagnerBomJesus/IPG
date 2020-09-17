a96cfcbba6f57ad140b99c67332e9902fb9101f2
Vagner Bom Jesus <vagneripg@gmail.com>
2020-09-17 01:57:32 +0100

    Alerta de saida da app
    para verificar se o ussuario deseja realmete sair da app

--- app/src/main/java/stp/vagnerjesus/ipg/MainActivity.java
+++ app/src/main/java/stp/vagnerjesus/ipg/MainActivity.java
@@ -1,7 +1,9 @@
 package stp.vagnerjesus.ipg;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    //Caixa de dialogo para sair
+    public void onBackPressed(){
+        AlertDialog.Builder alertDialogBuilder =new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(getString(R.string.Comfirme));
+        alertDialogBuilder.setIcon(R.drawable.ic_exit);
+        alertDialogBuilder.setMessage(getString(R.string.realmenteSair));
+        alertDialogBuilder.setCancelable(false);
+        alertDialogBuilder.setPositiveButton("SIM", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+
+            }
+        });
+        alertDialogBuilder.setNegativeButton("NÃO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
