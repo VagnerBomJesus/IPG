66fe906416c3135ca942e4f3885c2a8ab2a4928f
Vagner Bom Jesus <vagneripg@gmail.com>
2020-09-17 18:45:01 +0100

    criação da novas Intentes

---
+++ app/src/main/java/stp/vagnerjesus/ipg/MoodleActivity.java
@@ -0,0 +1,14 @@
+package stp.vagnerjesus.ipg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoodleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_moodle);
+    }
+}
