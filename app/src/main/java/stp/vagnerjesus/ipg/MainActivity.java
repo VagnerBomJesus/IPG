0dd8e97a32e2537f2ac9fd0a501eaf4aefe130a1
Vagner Bom Jesus <vagneripg@gmail.com>
2020-09-20 01:02:30 +0100

    update caixa de dialogo
    formatar a caixa de dialogo que pede para finalizar a aplicação

--- app/src/main/java/stp/vagnerjesus/ipg/MainActivity.java
+++ app/src/main/java/stp/vagnerjesus/ipg/MainActivity.java
@@ -2,12 +2,18 @@ package stp.vagnerjesus.ipg;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     }
     //Caixa de dialogo para sair
     public void onBackPressed(){
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Light_Dialog_Alert);
+        /*AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this, android.R.style.Theme_Material_Light_Dialog_Alert);
        // AlertDialog.Builder alerta = new AlertDialog.Builder(this,
                 //android.R.style.Theme_Material_Light_Dialog_Alert);
         alertDialogBuilder.setTitle(getString(R.string.Comfirme));
@@ -41,9 +47,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();*/
+        showSuccessDialog();
+    }
+    private void showSuccessDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this, R.style.AlertDialogTheme);
+        View view = LayoutInflater.from(MainActivity.this).inflate(
+                R.layout.layout_dialog,
+                (ConstraintLayout) findViewById(R.id.layoutDialog_Constraint)
+        );
+        builder.setView(view);
+        ((TextView) view.findViewById(R.id.textTitulo)).setText(getResources().getString(R.string.Comfirme));
+        ((TextView) view.findViewById(R.id.dialgoDescrica)).setText(getResources().getString(R.string.realmenteSair));
+
+        ((Button) view.findViewById(R.id.buttonNo)).setText(getResources().getString(R.string.Nao));
+        ((Button) view.findViewById(R.id.buttonSi)).setText(getResources().getString(R.string.sim));
+
+        ((ImageView) view.findViewById(R.id.imagemAlerta)).setImageResource(R.drawable.iconipg);
+
+        final AlertDialog alertDialog = builder.create();
+
+        view.findViewById(R.id.buttonSi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                //Toast.makeText(MainActivity.this,"Sim", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
+        view.findViewById(R.id.buttonNo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                //Toast.makeText(MainActivity.this,"Não", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        if(alertDialog.getWindow() !=null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+
+        }
         alertDialog.show();
     }
-
     public void onclikIpg(View view) {//botão "IPG"
         Intent i = new Intent(this, IpgActivity.class);
         startActivity(i);
